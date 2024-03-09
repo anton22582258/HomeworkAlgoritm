5170254d42e679fa74707ab79d1fa98f49d42708
Anton <anton22582258@gmail.com>
2024-03-09 21:52:24 +0300

    HomeworkAlgoritm сделано

---
+++ src/main/java/org/example/exception/ListIsFullException.java
@@ -0,0 +1,25 @@
+package org.example.exception;
+
+public class ListIsFullException extends RuntimeException {
+    public ListIsFullException() {
+    }
+
+    public ListIsFullException(String message) {
+        super(message);
+    }
+
+    public ListIsFullException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ListIsFullException(Throwable cause) {
+        super(cause);
+    }
+
+    public ListIsFullException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
+
+
+
